3609eaf31fdbf5c50296310b5f0e5ae7d45f93fd
Caleb Parrra <mccalebyt@gmail.com>
2022-07-15 08:46:19 -0500

    'configs'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Logo from '../public/Logo.png'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-black'>
+    <div className='bg-black w-screen h-screen '>
       <Head>
         <title>hola</title>
       </Head>
+
+      <Image src={Logo} alt='logo' />
     </div>
   )
 }
