8072bb2e2a434316005809da529e622bf7b2fd84
Caleb Parrra <mccalebyt@gmail.com>
2022-07-16 16:42:17 -0500

    'navbar'

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,69 @@
 import Image from "next/image"
 import Logo from '../public/Logo.png'
-import { AiOutlineMenu } from 'react-icons/ai'
+import Link from "next/link"
 
-const Header = () =>{
-    
-    return(
+import { useState } from "react"
+
+const Header = () => {
+    const [active, setActive] = useState(false);
+
+    const handleClick = () => {
+        setActive(!active);
+    };
+    return (
         <>
-            <nav className="flex justify-between px-5 justify-items-center mt-5">
-                <Image src={Logo}></Image>
-                <div className="absolute flex flex-col md:flex gap-5">
-                    <a className='text-white'>WORK</a>
-                    <a className='text-white'>SERVICES</a>
-                    <a className='text-white'>CONTACT</a>
+            <nav className='flex items-center flex-wrap p-3 '>
+
+                <a className='inline-flex items-center p-2 mr-4 '>
+                    <Image src={Logo}></Image>
+                </a>
+
+                <button
+                    className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+                    onClick={handleClick}
+                >
+                    <svg
+                        className='w-6 h-6'
+                        fill='none'
+                        stroke='currentColor'
+                        viewBox='0 0 24 24'
+                        xmlns='http://www.w3.org/2000/svg'
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth={2}
+                            d='M4 6h16M4 12h16M4 18h16'
+                        />
+                    </svg>
+                </button>
+
+                <div
+                    className={`${active ? '' : 'hidden'
+                        }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+                >
+                    <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+                        <Link href='/'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white  items-center justify-center hover:bg-green-600 hover:text-white '>
+                                WORK
+                            </a>
+                        </Link>
+
+
+                        <Link href='/'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white  items-center justify-center hover:bg-green-600 hover:text-white '>
+                                SERVICE
+                            </a>
+                        </Link>
+
+                        <Link href='/'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white  items-center justify-center hover:bg-green-600 hover:text-white '>
+                                CONTACT
+                            </a>
+                        </Link>
+
+                    </div>
                 </div>
-                <AiOutlineMenu  size={30} color="white" className='md:hidden'/>
             </nav>
         </>
     )
