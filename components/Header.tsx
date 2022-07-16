d714541a3024b7b1b0be6da9f390cb61410d88ab
Caleb Parrra <mccalebyt@gmail.com>
2022-07-16 15:45:15 -0500

    'fixing'

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import Logo from '../../public/Logo.png'
+import Logo from '../public/Logo.png'
 import { AiOutlineMenu } from 'react-icons/ai'
 
 const Header = () =>{
@@ -8,12 +8,12 @@ const Header = () =>{
         <>
             <nav className="flex justify-between px-5 justify-items-center mt-5">
                 <Image src={Logo}></Image>
-                <div className="hidden  md:flex-row md:flex">
+                <div className="absolute flex flex-col md:flex gap-5">
                     <a className='text-white'>WORK</a>
                     <a className='text-white'>SERVICES</a>
                     <a className='text-white'>CONTACT</a>
                 </div>
-                <AiOutlineMenu size={30} color="white" className='md:hidden'/>
+                <AiOutlineMenu  size={30} color="white" className='md:hidden'/>
             </nav>
         </>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Header from './components/Header'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className='w-screen h-screen '>
+    <div className=''>
       <Head>
         <title>ParraCode</title>
       </Head>
