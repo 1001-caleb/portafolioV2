6ff92da28eb2e4174ac55bd1d8ce06023f4bd41e
Caleb Parrra <mccalebyt@gmail.com>
2022-07-18 14:58:02 -0500

    Works

---
+++ components/Work.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+
+
+function Work() {
+
+   const works = [
+        {
+            id: 1,
+            img: 'project1',
+            title: '',
+            desc: '',
+            link: ''
+        },
+        {
+            id: 2,
+            img: '',
+            title: '',
+            desc: '',
+            link: ''
+        }
+   ]
+
+  return (
+    <section>
+        <h3 className='text-white text-4xl lg:text-7xl text-center mt-20'>WORK</h3>
+        <div>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Work
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,22 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import Header from '../components/Header'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import Work from "../components/Work";
 
 const Home: NextPage = () => {
   return (
-    <div className=''>
+    <div>
       <Head>
         <title>ParraCode</title>
-        <link rel='icon' href='/PC1.png'></link>
+        <link rel="icon" href="/PC1.png"></link>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Navbar />
       <Header />
-
+      <Work />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
