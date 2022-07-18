95f6960138e9c65a4da405f5d0a48a51b33b9985
Caleb Parrra <mccalebyt@gmail.com>
2022-07-18 17:43:35 -0500

    services cards

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,16 +1,55 @@
 
 
 function Services() {
-  return (
-    <>
-    <div className='p-5 '>
-        <h3 className='text-white text-4xl lg:text-7xl text-center mt-20 '>Services</h3>
-        <div>
+    const services = [
+        {
+            id: 1,
+            title: 'Figma designs',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur ',
+            price: '$$'
+        },
+        {
+            id: 2,
+            title: 'Web design',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur ',
+            price: '$$'
+        },
+        {
+            id: 3,
+            title: 'Discord bot development',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur ',
+            price: '$$'
+        },
+        {
+            id: 4,
+            title: 'Mentoring',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur ',
+            price: '$$'
+        },
 
-        </div>
-    </div>
-</>
-  )
+
+    ]
+
+    return (
+        <>
+            <div className='p-5 '>
+                <h3 className='text-white text-4xl lg:text-7xl text-center mt-20 '>Services</h3>
+                <div className='flex flex-wrap lg:flex-nowrap gap-5 mt-10'>
+                    {
+                        services.map(service =>{
+                            return(
+                                <div className='bg-white w-max h-70 p-10'> 
+                                    <h3 className='text-2xl mb-5'>{service.title}</h3>
+                                    <p className=''>{service.desc}</p>
+                                    <p className='bg-cyan-500 absolute object-bottom'>{service.price}</p>
+                                </div>
+                            ) 
+                        })
+                    }
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Services
