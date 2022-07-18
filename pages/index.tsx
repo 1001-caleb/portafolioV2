d217542a5e00cfcb8797d26be18a903a186a3c9a
Caleb Parrra <mccalebyt@gmail.com>
2022-07-18 09:13:11 -0500

    nv

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Header from '../components/Header'
+import Header from '../components/Navbar'
+
 
 const Home: NextPage = () => {
   return (
     <div className=''>
       <Head>
         <title>ParraCode</title>
+        <link rel='icon' href='../public/PC.png'></link>
       </Head>
-      <Header></Header>
+      <Header />
 
     </div>
   )
