9131318e2df31ba032f75b9e835fce4ce3d0d312
Caleb Parrra <mccalebyt@gmail.com>
2022-08-03 09:41:39 -0500

    projects dinamic routess

---
+++ pages/projects/[name].tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+
+export default function Project(projects: any) {
+    console.log(projects);
+    return (
+        <>
+            <Head>
+                <title>{projects.name}</title>
+            </Head>
+            <div>
+                <h1>{projects.name}</h1>
+                <p>{projects.description}</p>
+            </div>
+        </>
+
+    )
+}
+
+export async function getStaticPaths() {
+    const res = await fetch('http://localhost:3000/api/projects');
+    const projects = await res.json();
+    const paths = projects.map((project: any) => ({
+        params: { name: project.name }
+    }));
+    return { paths, fallback: false };
+}
+export async function getStaticProps(context: any) {
+    const { name } = await context.params;
+
+    const res = await fetch(`http://localhost:3000/api/projects/${name}`);
+    const projects = await res.json();
+    console.log(projects);
+    return {
+        props: projects
+
+    }
+}
