b9ecd3f8606c1f34fc5d041002ffb589b5125a49
Caleb Parrra <mccalebyt@gmail.com>
2022-08-05 10:01:19 -0500

    fix navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -59,7 +59,7 @@ const Header = () => {
                     className={`${active ? '' : 'hidden'
                         }   w-full lg:inline-flex lg:flex-grow lg:w-auto `}
                 >
-                    <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto bg-black  bg-opacity-30 backdrop-blur-sm translate-y-5'>
+                    <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
                         <Link href='#work'>
                             <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white  items-center justify-center  hover:text-violet-600 '>
                                 WORK
