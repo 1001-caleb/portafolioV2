1dfe04f0f56706f8a9a5e09b947acbce4f4669d3
Caleb Parrra <mccalebyt@gmail.com>
2022-08-05 19:54:35 -0500

    preloader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
+import LoadingScreen from '../components/LoadingScreen';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
-  return <Component {...pageProps} />
+  const  [loading, setLoading]  = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  return(
+    <>
+      {!loading ? (<Component {...pageProps} />) : (<LoadingScreen /> )}
+    </>
+   
+  ) 
 }
 
 export default MyApp
