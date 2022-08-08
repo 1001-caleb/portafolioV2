c6bc1c5ff1fbf1352fa391eb8eea490a6e730055
Caleb Parrra <mccalebyt@gmail.com>
2022-08-08 09:36:38 -0500

    loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,48 +1,23 @@
-import '../styles/globals.css'
-import '../styles/screen.css';
-import '../styles/balls.css';
 
 import type { AppProps } from 'next/app'
 import { useEffect, useState } from 'react'
 import LoadingScreen from '../components/LoadingScreen';
-import Router, { useRouter } from 'next/router';
+import '../styles/globals.css'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState<boolean>(true);
-  const router = useRouter();
-
+ 
   useEffect(() => {
-    router.push('/');
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000); 
   }, []);
 
-  useEffect(() => {
-    router.events.on('routeChangeStart', () => {
-      setLoading(true);
-    });
-    router.events.on('routeChangeComplete', () => {
-      setLoading(false);
-    });
-    router.events.on('routeChangeError', () => {
-      setLoading(false);
-    });
-
-    return () => {
-     router.events.off('routeChangeStart', () => {
-      setLoading(true);
-      });
-      router.events.off('routeChangeComplete', () => {
-        setLoading(false);
-      });
-      router.events.off('routeChangeError', () => {
-        setLoading(false);
-      });
-    }
-  }, [router.events]);
 
   return (
     <>
-      {!loading ? (<Component {...pageProps} />) : (<LoadingScreen />)}
+      {loading ? (<LoadingScreen />) : (<Component {...pageProps} />)}
     </>
 
   )
